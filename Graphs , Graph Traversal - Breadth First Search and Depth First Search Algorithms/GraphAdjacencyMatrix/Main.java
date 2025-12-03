b769e01570701ca98329a6de49a761c10c5851d6
devanshimehrotrahub <devanshimehrotra2025@gmail.com>
2025-12-03 17:19:06 +0530

    Refactor main method for clarity and remove comments

--- Graphs , Graph Traversal - Breadth First Search and Depth First Search Algorithms/GraphAdjacencyMatrix/Main.java
+++ Graphs , Graph Traversal - Breadth First Search and Depth First Search Algorithms/GraphAdjacencyMatrix/Main.java
@@ -1,52 +1,50 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String args[]) {
-        ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+  public static void main(String args[]) {
+    ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+    nodeList.add(new GraphNode("A", 0));
+    nodeList.add(new GraphNode("B", 1));
+    nodeList.add(new GraphNode("C", 2));
+    nodeList.add(new GraphNode("D", 3));
+    nodeList.add(new GraphNode("E", 4));
 
-        nodeList.add(new GraphNode("A", 0));
-        nodeList.add(new GraphNode("B", 1));
-        nodeList.add(new GraphNode("C", 2));
-        nodeList.add(new GraphNode("D", 3));
-        nodeList.add(new GraphNode("E", 4));
+    Graph g = new Graph(nodeList);
 
-        Graph g = new Graph(nodeList);
+    g.addUndirectedEdge(0, 1);
+    g.addUndirectedEdge(0, 2);
+    g.addUndirectedEdge(0, 3);
+    g.addUndirectedEdge(1, 4);
+    g.addUndirectedEdge(2, 3);
+    g.addUndirectedEdge(3, 4);
 
-        g.addUndirectedEdge(0, 1);
-        g.addUndirectedEdge(0, 2);
-        g.addUndirectedEdge(0, 3);
-        g.addUndirectedEdge(1, 4);
-        g.addUndirectedEdge(2, 3);
-        g.addUndirectedEdge(3, 4);
+    g.printGraph();
+    g.bfs();
+    // g.dfs();
 
-        g.printGraph();
-        g.bfs();
-        // g.dfs();
+    // ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+    // nodeList.add(new GraphNode("A", 0));
+    // nodeList.add(new GraphNode("B", 1));
+    // nodeList.add(new GraphNode("C", 2));
+    // nodeList.add(new GraphNode("D", 3));
+    // nodeList.add(new GraphNode("E", 4));
+    // nodeList.add(new GraphNode("F", 5));
+    // nodeList.add(new GraphNode("G", 6));
 
-        // ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
-        // nodeList.add(new GraphNode("A", 0));
-        // nodeList.add(new GraphNode("B", 1));
-        // nodeList.add(new GraphNode("C", 2));
-        // nodeList.add(new GraphNode("D", 3));
-        // nodeList.add(new GraphNode("E", 4));
-        // nodeList.add(new GraphNode("F", 5));
-        // nodeList.add(new GraphNode("G", 6));
+    // Graph g = new Graph(nodeList);
 
-        // Graph g = new Graph(nodeList);
+    // g.addUndirectedEdge(0, 1);
+    // g.addUndirectedEdge(0, 2);
+    // g.addUndirectedEdge(1, 3);
+    // g.addUndirectedEdge(1, 6);
+    // g.addUndirectedEdge(2, 3);
+    // g.addUndirectedEdge(2, 4);
+    // g.addUndirectedEdge(3, 5);
+    // g.addUndirectedEdge(4, 5);
+    // g.addUndirectedEdge(5, 6);
 
-        // g.addUndirectedEdge(0, 1);
-        // g.addUndirectedEdge(0, 2);
-        // g.addUndirectedEdge(1, 3);
-        // g.addUndirectedEdge(1, 6);
-        // g.addUndirectedEdge(2, 3);
-        // g.addUndirectedEdge(2, 4);
-        // g.addUndirectedEdge(3, 5);
-        // g.addUndirectedEdge(4, 5);
-        // g.addUndirectedEdge(5, 6);
-
-        // g.printList();
-        // g.bfs();
-        // g.dfs();
-
-    }
+    // g.printGraph();
+    // g.bfs();
+    // g.dfs();
+  }
 }
